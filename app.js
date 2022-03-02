3465757c4dfeeebe1a2825a640cb8249a3b11926
Emon02 <abdulalimemon11@gmail.com>
2022-03-02 22:46:03 +0600

    complete

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const searchPhone = () => {
 
     // clear Data
     searchInput.value = "";
+
     // No product found message (h2 tag) 
     const message = document.getElementById("message");
+    const cardSingleProduct = document.getElementById("single-product");
+
     if (searchInputValue == "") {
         message.style.display = "block";
+        cardSingleProduct.textContent = "";
     } else {
         message.style.display = "none";
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchInputValue}`;
@@ -19,23 +23,23 @@ const searchPhone = () => {
 }
 
 const displaySearchResults = searchResults => {
-    // console.log(searchResults);
-
     // targeting card div
     const cardAllProduct = document.getElementById("all-product");
     // clear Data 
-    cardAllProduct.textContent ="";
+    cardAllProduct.textContent = "";
+
     // No product found message (h2 tag)
     const message = document.getElementById("message");
-    if(searchResults.length == 0){
+    const cardSingleProduct = document.getElementById("single-product");
+    if (searchResults.length == 0) {
         message.style.display = "block";
-    }else{
+        cardSingleProduct.textContent = "";
+    } else {
         message.style.display = "none";
-
-        const productSlice = searchResults.slice(0,20);
-
+        cardSingleProduct.textContent = "";
+        // slice product
+        const productSlice = searchResults.slice(0, 20);
         productSlice.forEach(result => {
-            // result.slice(0,20);
             // all product card
             const div = document.createElement("div");
             div.classList.add("col");
@@ -51,22 +55,25 @@ const displaySearchResults = searchResults => {
             `;
             cardAllProduct.appendChild(div);
         });
-        
     }
 }
+// load data
 const loadSingleProduct = product => {
     const url = `https://openapi.programming-hero.com/api/phone/${product}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displaySingleProduct(data.data));
 }
+
 const displaySingleProduct = singleProduct => {
     const cardSingleProduct = document.getElementById("single-product");
     // clear data
-    cardSingleProduct.textContent ="";
+    cardSingleProduct.textContent = "";
+    const cardAllProduct = document.getElementById("all-product");
+    cardAllProduct.textContent = "";
+
     // Single Product details card
     const div = document.createElement("div");
-
     div.classList.add("row");
     div.innerHTML = `
         <div class="col-12 col-md-4">
@@ -77,39 +84,36 @@ const displaySingleProduct = singleProduct => {
         <div class="col-12 col-md-8">
             <div class="card-body">
                 <h4 class="card-title">${singleProduct.name}</h4>
-                <h6 class="card-title">Release Date: ${singleProduct.releaseDate ? singleProduct.releaseDate : 'Information Not Available' }</h6>
+                <h6 class="card-title">Release Date: ${singleProduct.releaseDate ? singleProduct.releaseDate : 'Information Not Available'}</h6>
                 <h5 class="card-title">Main Features</h5>
-                <p class="card-title"><span class="fw-bold">Storage : </span>${singleProduct.mainFeatures.storage ? singleProduct.mainFeatures.storage : 'Information Not Available' } </p>
+                <p class="card-title"><span class="fw-bold">Storage : </span>${singleProduct.mainFeatures.storage ? singleProduct.mainFeatures.storage : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">Display Size : </span>
-                ${singleProduct.mainFeatures.displaySize ? singleProduct.mainFeatures.displaySize : 'Information Not Available' } </p>
+                ${singleProduct.mainFeatures.displaySize ? singleProduct.mainFeatures.displaySize : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">ChipSet : </span>
-                ${singleProduct.mainFeatures.chipSet ? singleProduct.mainFeatures.chipSet : 'Information Not Available' } </p>
+                ${singleProduct.mainFeatures.chipSet ? singleProduct.mainFeatures.chipSet : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">Memoey : </span>
-                ${singleProduct.mainFeatures.memory ? singleProduct.mainFeatures.memory : 'Information Not Available' } </p>
+                ${singleProduct.mainFeatures.memory ? singleProduct.mainFeatures.memory : 'Information Not Available'} </p>
 
                 <h5 class="card-title">Sensors</h5>
                 <p class="card-title"><span class="fw-bold">Sensors : </span>
-                ${ Object.values(singleProduct.mainFeatures.sensors) ? Object.values(singleProduct.mainFeatures.sensors) : 'Information Not Available' } </p>
+                ${Object.values(singleProduct.mainFeatures.sensors) ? Object.values(singleProduct.mainFeatures.sensors) : 'Information Not Available'} </p>
 
                 <h5 class="card-title">Others</h5>
                 <p class="card-title"><span class="fw-bold">Bluetooth : </span>
-                ${singleProduct.others.Bluetooth ? singleProduct.others.Bluetooth : 'Information Not Available' } </p>
+                ${singleProduct.others.Bluetooth ? singleProduct.others.Bluetooth : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">GPS : </span>
-                ${singleProduct.others.GPS ? singleProduct.others.GPS : 'Information Not Available' } </p>
+                ${singleProduct.others.GPS ? singleProduct.others.GPS : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">NFC : </span>
-                ${singleProduct.others.NFC ? singleProduct.others.NFC : 'Information Not Available' } </p>
+                ${singleProduct.others.NFC ? singleProduct.others.NFC : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">Radio : </span>
-                ${singleProduct.others.Radio ? singleProduct.others.Radio : 'Information Not Available' } </p>
+                ${singleProduct.others.Radio ? singleProduct.others.Radio : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">USB : </span>
-                ${singleProduct.others.USB ? singleProduct.others.USB : 'Information Not Available' } </p>
+                ${singleProduct.others.USB ? singleProduct.others.USB : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">WLAN : </span>
-                ${singleProduct.others.WLAN ? singleProduct.others.WLAN : 'Information Not Available' } </p>
+                ${singleProduct.others.WLAN ? singleProduct.others.WLAN : 'Information Not Available'} </p>
             </div>
         </div>
     `;
     cardSingleProduct.appendChild(div);
 }
-
-
-
 
