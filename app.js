e79b4054523d44b0f67a9336bba63819a072e160
Emon02 <abdulalimemon11@gmail.com>
2022-03-17 13:58:58 +0600

    Bug fixed

--- app.js
+++ app.js
@@ -78,39 +78,39 @@ const displaySingleProduct = singleProduct => {
     div.innerHTML = `
         <div class="col-12 col-md-4">
             <div class="text-center">
-                <img src="${singleProduct.image}" class="w-75 mx-auto py-4" alt="Image">
+                <img src="${singleProduct?.image}" class="w-75 mx-auto py-4" alt="Image">
             </div>
         </div>
         <div class="col-12 col-md-8">
             <div class="card-body">
-                <h4 class="card-title">${singleProduct.name}</h4>
-                <h6 class="card-title">Release Date: ${singleProduct.releaseDate ? singleProduct.releaseDate : 'Information Not Available'}</h6>
+                <h4 class="card-title">${singleProduct?.name}</h4>
+                <h6 class="card-title">Release Date: ${singleProduct?.releaseDate ? singleProduct?.releaseDate : 'Information Not Available'}</h6>
                 <h5 class="card-title">Main Features</h5>
-                <p class="card-title"><span class="fw-bold">Storage : </span>${singleProduct.mainFeatures.storage ? singleProduct.mainFeatures.storage : 'Information Not Available'} </p>
+                <p class="card-title"><span class="fw-bold">Storage : </span>${singleProduct?.mainFeatures?.storage ? singleProduct?.mainFeatures?.storage : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">Display Size : </span>
-                ${singleProduct.mainFeatures.displaySize ? singleProduct.mainFeatures.displaySize : 'Information Not Available'} </p>
+                ${singleProduct?.mainFeatures?.displaySize ? singleProduct?.mainFeatures?.displaySize : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">ChipSet : </span>
-                ${singleProduct.mainFeatures.chipSet ? singleProduct.mainFeatures.chipSet : 'Information Not Available'} </p>
+                ${singleProduct?.mainFeatures?.chipSet ? singleProduct?.mainFeatures?.chipSet : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">Memoey : </span>
-                ${singleProduct.mainFeatures.memory ? singleProduct.mainFeatures.memory : 'Information Not Available'} </p>
+                ${singleProduct?.mainFeatures?.memory ? singleProduct?.mainFeatures?.memory : 'Information Not Available'} </p>
 
                 <h5 class="card-title">Sensors</h5>
                 <p class="card-title"><span class="fw-bold">Sensors : </span>
-                ${Object.values(singleProduct.mainFeatures.sensors) ? Object.values(singleProduct.mainFeatures.sensors) : 'Information Not Available'} </p>
+                ${Object.values(singleProduct?.mainFeatures?.sensors) ? Object.values(singleProduct?.mainFeatures?.sensors) : 'Information Not Available'} </p>
 
                 <h5 class="card-title">Others</h5>
                 <p class="card-title"><span class="fw-bold">Bluetooth : </span>
-                ${singleProduct.others.Bluetooth ? singleProduct.others.Bluetooth : 'Information Not Available'} </p>
+                ${singleProduct?.others?.Bluetooth ? singleProduct?.others?.Bluetooth : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">GPS : </span>
-                ${singleProduct.others.GPS ? singleProduct.others.GPS : 'Information Not Available'} </p>
+                ${singleProduct?.others?.GPS ? singleProduct?.others?.GPS : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">NFC : </span>
-                ${singleProduct.others.NFC ? singleProduct.others.NFC : 'Information Not Available'} </p>
+                ${singleProduct?.others?.NFC ? singleProduct?.others?.NFC : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">Radio : </span>
-                ${singleProduct.others.Radio ? singleProduct.others.Radio : 'Information Not Available'} </p>
+                ${singleProduct?.others?.Radio ? singleProduct?.others?.Radio : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">USB : </span>
-                ${singleProduct.others.USB ? singleProduct.others.USB : 'Information Not Available'} </p>
+                ${singleProduct?.others?.USB ? singleProduct?.others?.USB : 'Information Not Available'} </p>
                 <p class="card-title"><span class="fw-bold">WLAN : </span>
-                ${singleProduct.others.WLAN ? singleProduct.others.WLAN : 'Information Not Available'} </p>
+                ${singleProduct?.others?.WLAN ? singleProduct?.others?.WLAN : 'Information Not Available'} </p>
             </div>
         </div>
     `;
